b281640296d3ea2d490bc5014ebe9c967e3958b6
Kevin Rodriguez <iamkevnrodriguez@gmail.com>
2020-04-24 17:50:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   count = 0
   new_array = []
   while count < array.length do
-    new_array.push(array[0]**array[0])
+    new_array.push(array.each do |i| i ** 2)
     count += 1
   end
   return new_array
